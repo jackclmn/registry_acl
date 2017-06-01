17f2bbbde37d4d308a4505eef23efa5a54d55c38
ipcrm <mcadorette@gmail.com>
2017-06-01 11:33:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+registry_acl
+============
+
+This module is used to set registry permissions and audit info.
+
+WIP!
---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+define registry_acl (
+  Hash    $ace_hash,
+  String  $regkey = $title,
+  Boolean $purge = false,
+  Boolean $inherit_from_parent = true,
+){
+
+  $ace_hash.each |$k,$r| {
+    assert_type(String, $r['secprincipal'])
+    assert_type(String, $r['inheritance'])
+    assert_type(String, $r['propagation'])
+    assert_type(String, $r['objaccess'])
+    assert_type(String, $r['objtype'])
+    assert_type(Boolean, $r['remove_ace'])
+
+    if ($r['purge'] == true) and ($r['remove_ace'] == true) {
+      raise('You cannot set both purge and remove_ace to true!')
+    }
+  }
+
+  exec {"updating acl for: ${title}":
+    unless   => template('registry_acl/check_acl.ps1.erb'),
+    command  => template('registry_acl/set_acl.ps1.erb'),
+    provider => powershell,
+  }
+
+}
